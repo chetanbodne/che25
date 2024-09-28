de054574e234a0990e090870dd5edaf9b4f4f182
chetangitdemo <chetanbodne7294@gmail.com>
2024-09-28 14:47:58 +0530

    defect is change

---
+++ src/main/java/com/example/B.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import ch.qos.logback.core.encoder.EchoEncoder;
+
+public class B {
+    public static void main(String[] args) {
+        System.out.println("chetan");
+    }
+
+
+
+}
